fb9612f98748d31837c8a834aa86673ab7e71bf8
Alexandr <sashkaageev@gmail.com>
2026-02-08 16:39:16 +0300

    fix method name

--- internal/http/handlers/create.go
+++ internal/http/handlers/create.go
@@ -10,7 +10,7 @@ import (
 )
 
 type OrderService interface {
-	ProduceOrder(ctx context.Context, cmd *model.CreateOrderCommand) (*model.Order, error)
+	Create(ctx context.Context, cmd *model.CreateOrderCommand) (*model.Order, error)
 }
 
 type createRequest struct {
@@ -33,7 +33,7 @@ func Create(service OrderService) http.HandlerFunc {
 			return
 		}
 
-		order, err := service.ProduceOrder(r.Context(), &model.CreateOrderCommand{
+		order, err := service.Create(r.Context(), &model.CreateOrderCommand{
 			UserID: req.UserID,
 			Items:  req.Items,
 		})
