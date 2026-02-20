757f43127d80f98fbe278d4688e4ef1b003744eb
Alexandr <sashkaageev@gmail.com>
2026-02-20 15:21:13 +0300

    added error handling for open cb

--- internal/kafka/handler/handler.go
+++ internal/kafka/handler/handler.go
@@ -3,13 +3,15 @@ package handler
 import (
 	"context"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"log/slog"
 
 	"github.com/confluentinc/confluent-kafka-go/v2/kafka"
 
-	"github.com/sanchey92/order-processor/internal/domain/errors"
+	er "github.com/sanchey92/order-processor/internal/domain/errors"
 	"github.com/sanchey92/order-processor/internal/domain/model"
+	"github.com/sanchey92/order-processor/pkg/breaker"
 )
 
 type OrderProcessor interface {
@@ -31,15 +33,18 @@ func NewKafkaHandler(op OrderProcessor, l *slog.Logger) *KafkaHandler {
 func (h *KafkaHandler) Handle(ctx context.Context, msg *kafka.Message) error {
 	var cmd model.CreateOrderCommand
 	if err := json.Unmarshal(msg.Value, &cmd); err != nil {
-		return &errors.NonRetriableError{Cause: fmt.Errorf("invalid payload: %w", err)}
+		return &er.NonRetriableError{Cause: fmt.Errorf("invalid payload: %w", err)}
 	}
 	orderID := headerValue(msg.Headers, "order-id")
 	if orderID == "" {
-		return &errors.NonRetriableError{Cause: fmt.Errorf("missing order-id header")}
+		return &er.NonRetriableError{Cause: fmt.Errorf("missing order-id header")}
 	}
 
 	if err := h.orderProcessor.ProcessCommand(ctx, &cmd, orderID); err != nil {
-		// TODO: handle circuit breaker error
+		// Circuit breaker open -> retriable, will come back
+		if errors.Is(err, breaker.ErrOpen) {
+			return &er.RetriableError{Cause: err}
+		}
 
 		// Saga failed but compensated → system is consistent.
 		h.logger.Error("saga failed (compensated)", slog.String("order_id", orderID), slog.Any("error", err))
